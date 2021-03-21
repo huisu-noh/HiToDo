bd52888cbba72b35f242fee5a493d82183dd6aec
huisu-noh <77339018+huisu-noh@users.noreply.github.com>
2021-03-21 13:39:23 +0900

    clock & greeting

---
+++ clock.js
@@ -0,0 +1,27 @@
+const clockTitle = document.querySelector(".clock-main")
+
+
+
+function clock(){
+    const date = new Date();
+    const month = date.getMonth();
+    const clockDate = date.getDate();
+    const day = date.getDay();
+    const week = ["Mon", "Tue", "Wen", "Thu", "Fri", "Sat", "Sun"];
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    clockTitle.innerText = `${month + 1}월 ${clockDate}일 ${week[day]}` + 
+    ` ${hours < 10 ? `0${hours}` : hours} : ${
+        minutes < 10 ? `0${minutes}` : minutes} : ${
+        seconds < 10 ? `0${seconds}` : seconds}`;
+}
+
+function init(){
+    clock();
+    setInterval(clock, 1000);
+}
+
+
+init();
---
+++ userName.js
@@ -0,0 +1,45 @@
+const form = document.querySelector(".js-form"),
+      input = form.querySelector("input"),
+      greetings = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser";
+const SHOWING_CN = "showing";
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}
+
+function paintGreeting(text){
+    input.classList.remove(SHOWING_CN);
+    greetings.classList.add(SHOWING_CN);
+    greetings.innerText = `Hello ${text}`;
+}
+
+
+function askForName() {
+    input.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
+function loadName(){
+    const currentUser = localStorage.getItem(USER_LS);
+    if(currentUser === null){
+        askForName();
+    } else {
+        paintGreeting(currentUser);
+    }
+}
+
+
+function init(){
+    loadName();
+}
+
+init();
